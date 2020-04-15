8f4324fcc7db3de33213fbc95a3dd90db64fc7a6
v.mohl <viktor.mohl@epikur.de>
2020-04-15 08:08:04 +0200

    pass data to parent element by using custom event @Output() decorator
    + use EventEmitter and emit()-Method for this
    + use alias customer event with @Output('alias')
    + use 'two-way data binding' for getting the input from template into application

--- cmp-databinding-final/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-final/src/app/cockpit/cockpit.component.ts
@@ -11,31 +11,48 @@ export class CockpitComponent implements OnInit {
    * Note: Property & event binding on 'HTML elements' (native properties & events),
    * 'directives' (custom properties & events) and 'components' (custom properties & events)
    */
-  // newServerName: string = '';
-  // newServerContent: string = '';
-
+  // properties for two-way-data-binding
+  newServerName = '';
+  newServerContent = '';
+  // custom event for passing data to parent component
   @Output() serverCreated = new EventEmitter<ServerData>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<ServerData>();
 
-  @ViewChild('serverContentInput', { static: false }) serverContentInput: ElementRef;
+  // @ViewChild('serverContentInput', { static: false }) serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer(nameInput: HTMLInputElement) {
-    this.serverCreated.emit({
-      serverName: nameInput.value,
-      serverContent: this.serverContentInput.nativeElement.value
-    });
+  onAddServer() {
+    // emitting the event ==> start communication between components
+    this.serverCreated.emit(this.createServerDataFromInput());
   }
 
-  onAddBlueprint(nameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({
-      serverName: nameInput.value,
-      serverContent: this.serverContentInput.nativeElement.value
-    });
+  onAddBlueprint() {
+    this.blueprintCreated.emit(this.createServerDataFromInput());
   }
+
+  private createServerDataFromInput(): ServerData{
+    return {
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    };
+  }
+
+  // onAddServer(nameInput: HTMLInputElement) {
+  //   this.serverCreated.emit({
+  //     serverName: nameInput.value,
+  //     serverContent: this.serverContentInput.nativeElement.value
+  //   });
+  // }
+  //
+  // onAddBlueprint(nameInput: HTMLInputElement) {
+  //   this.blueprintCreated.emit({
+  //     serverName: nameInput.value,
+  //     serverContent: this.serverContentInput.nativeElement.value
+  //   });
+  // }
 
 }
